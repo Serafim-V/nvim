cf8699ae3e5dd43f62af50bdbb3ba935a6af216f
ivan.vorotilin <ivan.vorotilin@sbermarket.ru>
2023-04-04 14:48:43 +0500

    man settings

--- init.lua
+++ init.lua
@@ -1,2 +1,20 @@
-require('plugins')
-require('general')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+print(lazypath)
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("keys")
+require("options")
+require("lazyp")
+
+--vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("tokyonight")
